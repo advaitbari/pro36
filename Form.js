02e6a0b9c7d3bec4f2b45d5145a8cce92194dec0
advaitbari <72794521+advaitbari@users.noreply.github.com>
2021-04-07 13:52:16 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,37 @@
+class Form {
+    constructor() {
+      
+    }
+  
+    display(){
+      var title = createElement('h2')
+      title.html("virtual pet");
+      title.position(130, 0);
+      
+      var input = createInput("Name");
+      
+      input.position(130, 160);
+      
+  
+      button.mousePressed(function(){
+        input.hide();
+        button.hide();
+  
+        var name = input.value();
+        if(mousePressed(addFood)){
+        feedTime.update();
+        }
+        if(mousePressed(Feed)){
+          food.update();
+          }
+
+        playerCount+=1;
+        player.update(name)
+        player.updateCount(playerCount);
+        var greeting = createElement('h3');
+        greeting.html("Hello " + name )
+        greeting.position(130, 160)
+      });
+  
+    }
+  }
